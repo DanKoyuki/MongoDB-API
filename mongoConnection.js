1719215fbd04a229a437be6a87520578b67d5ae3
Dan Koyuki <arashikoyuki8@gmail.com>
2023-11-17 19:18:33 +0700

    Testing

---
+++ mongoConnection.js
@@ -0,0 +1,25 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+async function connectToMongoDB(pConnectionString) {
+  const client = new MongoClient(pConnectionString, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    console.log("Connected to MongoDB Atlas");
+  } catch (e) {
+    console.error(e);
+  } 
+
+}
+
+module.export = connectToMongoDB;
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const connectToMongoDB = require('./connectToMongoDB');
+
+// API START HERE
+app.use(express.json());
+
+// Connecting to MongoDB
+let mongoConnected = false; // Flag to check MongoDB connection status
+
+// API endpoint to connect to MongoDB
+app.post('/connectToMongoDB', async (req, res) => {
+  try {
+    if (!mongoConnected) {
+      const uri = req.body.uri; // Assuming the URI is sent as 'uri' in the request body
+
+      if (!uri) {
+        return res.status(400).send('URI is missing in the request body');
+      }
+
+      // Connect to MongoDB Atlas if not already connected
+      await connectToMongoDB(uri);
+      mongoConnected = true;
+    }
+    res.send('Connected to MongoDB Atlas');
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Error connecting to MongoDB Atlas');
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
