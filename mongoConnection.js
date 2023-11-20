5f5f62fde3a294173ba88367b228cade314386d5
Dan Koyuki <arashikoyuki8@gmail.com>
2023-11-20 21:01:22 +0700

    update get list func

--- mongoConnection.js
+++ mongoConnection.js
@@ -94,7 +94,7 @@ async function getListCollection() {
     try {
       const collections = await selected_database.listCollections().toArray();
       const collectionNames = collections.collections.map(collect => collect.name);
-      console.log('Collections:', collectionNames); // Log the collection names
+      console.info({ message: 'Collections:', collections: collectionNames });
       return collectionNames;
     } catch (error) {
       console.error('Error fetching collections:', error);
